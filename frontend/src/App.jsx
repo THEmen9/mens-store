689f6eea1d84a6676a286898bd1cb0dbcebae22c
Ankit Kumar <122370470+THEmen9@users.noreply.github.com>
2026-09-06 21:36:00 +0530

    feat: set up frontend foundation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  
+
   return (
-    <>
-      <div>Men's-store</div>
-    </>
+    <Routes>
+      <Route path="/" element={<div>Men's-store</div>} />
+    </Routes>
   )
 }
 

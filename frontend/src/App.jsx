c160d58828b3aacc3519373ae056fda2cec7c03c
Ankit Kumar <122370470+THEmen9@users.noreply.github.com>
2026-09-13 20:07:33 +0530

    feat: add main layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import MainLayout from './layouts/MainLayout'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+     <MainLayout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </MainLayout>
   )
 }
 

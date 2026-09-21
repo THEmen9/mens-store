75006009c1c22a34708f428e853261486d017a25
Ankit Kumar <122370470+THEmen9@users.noreply.github.com>
2026-09-21 19:27:59 +0530

    feat: configure frontend api cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
+
 import productRoutes from "./routes/product.routes.js";
 import errorHandler from "./middleware/error.middleware.js";
 
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(express.json());
 

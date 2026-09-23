4eded74d322bd133a4412cae98f2043c97bfb5b8
Ankit Kumar <122370470+THEmen9@users.noreply.github.com>
2026-09-23 15:33:20 +0530

    feat: add user login

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -15,7 +15,22 @@ const register = async (req, res, next) => {
     next(error);
   }
 };
+// login
+const login = async (req, res, next) => {
+  try {
+    const result = await authService.loginUser(req.body);
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default {
   register,
+  login,
 };
--- backend/services/auth.service.js
+++ backend/services/auth.service.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import User from "../models/User.js";
+import generateToken from "../utils/generateToken.js";
 
 const isValidEmail = (email) => {
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
@@ -52,6 +53,56 @@ const registerUser = async ({ name, email, password }) => {
     };
 };
 
+// login
+const loginUser = async ({ email, password }) => {
+    if (!email || !isValidEmail(email.trim())) {
+        const error = new Error("Please provide a valid email");
+        error.statusCode = 400;
+        throw error;
+    }
+
+    if (!password) {
+        const error = new Error("Password is required");
+        error.statusCode = 400;
+        throw error;
+    }
+
+    const normalizedEmail = email.trim().toLowerCase();
+
+    const user = await User.findOne({
+        email: normalizedEmail,
+    });
+
+    if (!user || !user.passwordHash) {
+        const error = new Error("Invalid email or password");
+        error.statusCode = 401;
+        throw error;
+    }
+
+    const isPasswordMatch = await bcrypt.compare(
+        password,
+        user.passwordHash
+    );
+
+    if (!isPasswordMatch) {
+        const error = new Error("Invalid email or password");
+        error.statusCode = 401;
+        throw error;
+    }
+
+    const token = generateToken(user._id.toString());
+
+    return {
+        token,
+        user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        },
+    };
+};
+
 export default {
   registerUser,
+  loginUser,
 };
